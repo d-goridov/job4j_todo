0c43e328be05c8936a41e0db80b5a33e79bdd664
d.goridov <dimagoridov@yandex.ru>
2022-12-06 22:20:55 +0300

    4. TODO list [#3786] fix mvn test

---
+++ src/test/java/ru/job4j/todo/persistent/HbmStoreTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.todo.persistent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HbmStoreTest {
+
+    @Test
+    void add() {
+    }
+}
