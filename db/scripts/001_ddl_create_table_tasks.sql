1e3bc84a6f615bc6b83b734fbd3a589a7aadbe5b
d.goridov <dimagoridov@yandex.ru>
2022-12-07 10:09:15 +0300

    Добавил комментарии к таблице tasks

--- db/scripts/001_ddl_create_table_tasks.sql
+++ db/scripts/001_ddl_create_table_tasks.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS tasks (
    created TIMESTAMP,
    done BOOLEAN
 );
+
+comment on table tasks is 'Задания';
+comment on column tasks.id is 'Идентификатор задания';
+comment on column tasks.description is 'Описание задания';
+comment on column tasks.created is 'Время и дата создания задания';
+comment on column tasks.done is 'Статус выполнения задания';
