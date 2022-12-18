899fc275d6e9628c81aeca6c73a90fcabb4adc8a
d.goridov <dimagoridov@yandex.ru>
2022-12-19 00:13:44 +0300

    0. ToOne [#6873] fix bugs

--- db/scripts/003_ddl_alter_table_fk_tasks.sql
+++ db/scripts/003_ddl_alter_table_fk_tasks.sql
@@ -3,4 +3,3 @@ ALTER TABLE tasks ADD CONSTRAINT fk_user FOREIGN KEY
 (user_id) REFERENCES users(id);
 
 COMMENT ON COLUMN tasks.user_id is 'Идентификатор пользователя';
-COMMENT ON CONSTRAINT fk_user ON tasks is 'Идентификатор пользователя';
